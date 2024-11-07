a66513bdc6f9f7498f00ecb3c62f91bb4f5e752e
Duy Hung <157467676+ngngduyhung2609@users.noreply.github.com>
2024-11-07 14:33:54 +0700

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -1,12 +1,12 @@
 const socket = io();
 let videoFeedUrl;
 
-
+// Hàm cập nhật danh sách vật thể
 socket.on('update_objects', function(data) {
     const objectList = document.getElementById('object-list');
     const objectCount = document.getElementById('object-count');
 
-    objectList.innerHTML = '';
+    objectList.innerHTML = ''; // Xóa danh sách hiện tại
 
     data.objects.forEach(function(object) {
         const li = document.createElement('li');
@@ -14,29 +14,31 @@ socket.on('update_objects', function(data) {
         objectList.appendChild(li);
     });
 
- 
+    // Cập nhật số lượng vật thể
     objectCount.textContent = data.count_object;
 
+    // Tự động cuộn xuống dưới
     objectList.scrollTop = objectList.scrollHeight;
 });
 
-
+// Đảm bảo rằng mã JavaScript sau sẽ chạy sau khi DOM được tải đầy đủ
 document.addEventListener("DOMContentLoaded", function() {
     const videoFeed = document.getElementById('video-feed');
     const toggleCameraButton = document.getElementById('toggle-camera');
-    let cameraActive = true;
+    let cameraActive = true; // Biến theo dõi trạng thái camera
+
+    // Lấy URL video feed từ thuộc tính data của phần tử video-feed
     videoFeedUrl = videoFeed.dataset.src;
 
+    // Xử lý sự kiện nút "Tắt Camera" và "Bật Camera"
     toggleCameraButton.addEventListener('click', function() {
         if (cameraActive) {
-            videoFeed.src = "";
-            toggleCameraButton.textContent = "Bật Camera";
+            videoFeed.src = ""; // Dừng video
+            toggleCameraButton.textContent = "Bật Camera"; // Thay đổi văn bản nút
         } else {
-            setTimeout(() => {
-                videoFeed.src = videoFeedUrl;
-            }, 200);
-            toggleCameraButton.textContent = "Tắt Camera";
+            videoFeed.src = videoFeedUrl; // Bật lại video
+            toggleCameraButton.textContent = "Tắt Camera"; // Thay đổi văn bản nút
         }
-        cameraActive = !cameraActive;
+        cameraActive = !cameraActive; // Đảo trạng thái
     });
 });
